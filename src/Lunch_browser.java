631a3ed3abf56a8c68b4f247c94826d972fd9914
Alom <zohirul789@yahoo.com>
2017-07-30 03:25:18 -0400

    commit lunched

---
+++ src/Lunch_browser.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Lunch_browser {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://ceac.state.gov/IV/Login.aspx");
+		
+		Thread.sleep(3000);
+		
+		driver.quit();
+
+	}
+
+}
